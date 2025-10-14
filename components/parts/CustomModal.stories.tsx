62dc9fb9567204ced88da15fe8558bc4c5efae4f
KoseiMurata <kosei.murata@fox-hound.jp>
2025-10-14 12:37:23 +0900

    タスク3-2　ストーリーの作成

--- components/parts/CustomModal.stories.tsx
+++ components/parts/CustomModal.stories.tsx
@@ -3,11 +3,18 @@
 import React, { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import CustomModal from "./CustomModal";
-import CustomButton from "../../components/parts/CustomButton";
+import CustomButton from "./CustomButton";
 import { Box } from "@mui/material";
 
 // TODO: メタデータ
+const meta: Meta<typeof CustomModal> = {
+  title: "Parts/CustomModal",
+  component: CustomModal,
+};
+export default meta;
+
 // TODO: ストーリーの定義
+type Story = StoryObj<typeof CustomModal>;
 
 // TODO: デフォルトストーリーの作成
 export const Default: Story = {
@@ -16,15 +23,23 @@ export const Default: Story = {
 
     return (
       <Box>
-        // TODO: クリックでモーダル開閉させる
-        <CustomButton variantType="primary" onClick={}>
+         {/*TODO: クリックでモーダル開閉させる*/}
+        <CustomButton variantType="primary" onClick={() => setOpen(true)}>
           モーダルを開く
         </CustomButton>
         <CustomModal
         // TODO: Propを渡す
+        open={open}
+        title="確認"
+        content="この操作を実行しますか"
         // onCloceはsetOpenにfalseを渡す
+        onClose={() => setOpen(false)}
         // onConfirmはalert()を使ってクリックしたことを知らせて
         // setOpenにfalseを渡す
+        onConfirm={() => {
+          alert("確認ボタンが押されました");
+          setOpen(false);
+        }}
         />
       </Box>
     );
